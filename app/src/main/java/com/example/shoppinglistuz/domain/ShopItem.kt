34c99cafe3998d5f9e62809b68eb2a4b2abaff9a
SamsungIlnurio <nurich1995@mail.ru>
2022-10-09 19:47:55 +0500

    1. Добавили Data слой 2. Прописали в Data слое конкретную реализацию методов нашего приложения

--- app/src/main/java/com/example/shoppinglistuz/domain/ShopItem.kt
+++ app/src/main/java/com/example/shoppinglistuz/domain/ShopItem.kt
@@ -1,8 +1,21 @@
 package com.example.shoppinglistuz.domain
 
 data class ShopItem(
-    val id: Int,
+
     val name: String,
     val count: Int,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    var id: Int = UNDEFINED_ID
+) {
+
+/* ID = -1 - это жестко закодированное магиечкое число,
+плохая практика, так как никто не поймет что это за число.
+Это начальное ID для каждого элемента в списке покупок.
+Нужно создать константу:
+ */
+
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
+
